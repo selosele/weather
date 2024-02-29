f890397b53c07928229e656e8837958a38ecff8e
selosele <osh3384@naver.com>
2024-02-29 09:27:09 +0900

    리팩토링

--- index.php
+++ index.php
@@ -156,11 +156,11 @@
   <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <script>
-    // 페이징 처리 함수
-     function goPage(pageNo) {
-       $('#pageNo').val(pageNo);
-       $('#frm').submit();
-    }
+  // 페이징 처리 함수
+  function goPage(pageNo) {
+    $('#pageNo').val(pageNo);
+    $('#frm').submit();
+  }
 
     // var url = 'http://apis.data.go.kr/1360000/WthrWrnInfoService/getWthrWrnList'; /*URL*/
     // var queryParams = '?' + encodeURIComponent('serviceKey') + '='+ 'jU7M7fOuvtjbSX5pvEl652qe6cY6wETwJhA2CoR6SQCrOAi9gwuTNKUMHkouoxjehshxB95B3dUAH%2FBngrpFLA%3D%3D'; /*Service Key*/
